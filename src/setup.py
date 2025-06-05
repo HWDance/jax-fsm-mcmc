36127bc3cfb516839fa959fd4532103ac21219e7
Hugh Dance <danceh@hpc-gw1.hpc.swc.ucl.ac.uk>
2025-06-05 14:58:51 +0100

    setup

---
+++ src/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fsm_mcmc", 
+    version="0.1", 
+    author = "Hugh Dance",
+    author_email = "hughwdance@gmail.com",
+    packages=find_packages(),  
+)
