ab7579986eb6e142c4827acde697a83d127ff33f
Hugh Dance <danceh@hpc-gw2.hpc.swc.ucl.ac.uk>
2025-06-05 23:50:14 +0000

    Remove outdated setup.py from src/

--- src/setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="fsm_mcmc", 
-    version="0.1", 
-    author = "Hugh Dance",
-    author_email = "hughwdance@gmail.com",
-    packages=find_packages(),  
-)
