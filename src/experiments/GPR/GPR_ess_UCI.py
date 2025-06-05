83984dca93233997af4bf4b505639c8542844d88
Hugh Dance <danceh@hpc-gw1.hpc.swc.ucl.ac.uk>
2025-06-05 15:11:26 +0100

    GPR experiments

---
+++ src/experiments/GPR/GPR_ess_UCI.py
@@ -0,0 +1,97 @@
+# Imports
+import sys
+import jax
+import jax.numpy as jnp
+import jax.random as jrnd
+import pickle
+from pathlib import Path
+import blackjax
+
+# Data imports
+from ucimlrepo import fetch_ucirepo 
+import pandas as pd
+
+from FSM.mcmc.ellipticalslice_out import EllipticalSliceFSM
+from FSM.base.run_fsm_out import run_fsm
+from FSM.base.run_blackjax import run_blackjax
+from FSM.utils.gpr import logpdf_gp_fn as get_logpdf_fn
+
+def main(seed=0, data_id=477, num_chains=128, num_samples=1000, burn_in = 0, batch_fn = jax.vmap, num_train = None):
+
+    """ Constructing sampler inputs (rng, data, logprob, pos) """    
+
+    # RNG init
+    rng = jrnd.PRNGKey(seed)
+    pos_rng, rng = jrnd.split(rng, 2)
+      
+    # fetch dataset 
+    data = fetch_ucirepo(id=data_id) 
+      
+    # Determine ndata to use
+    if num_train is None:
+        num_train = len(jnp.array(data.data.targets)[:,0])
+      
+    # data extraction
+    X = jnp.array(data.data.features)[:num_train]
+    y = jnp.array(data.data.targets)[:num_train,0]
+
+    # Standardising
+    y = (y-y.mean())/y.std()
+    X = (X-X.mean(0))/X.std(0)
+
+    # Logprobfn
+    logprob_fn = jax.jit(get_logpdf_fn(y, X))
+    
+    # Initialisation
+    input_dims = 3
+    init_pos = jrnd.normal(pos_rng, shape = (num_chains, input_dims))
+    
+    """ ESS configs """
+    mean = jnp.zeros(input_dims)
+    cov = jnp.eye(input_dims)
+
+    """ FSM sampler run """
+    fsm = EllipticalSliceFSM(mean,cov)
+    fsm_time, fsm_ess, fsm_calls = run_fsm(
+        fsm,
+        logprob_fn,
+        rng,
+        init_pos,
+        input_dims,
+        num_samples, 
+        num_chains,
+        burn_in,
+        batch_fn,
+    )
+                                           
+    """ Standard sampler run """
+    kernel = blackjax.elliptical_slice(
+        logprob_fn, 
+        mean=mean, 
+        cov=cov
+    )
+    naive_time, naive_ess, naive_info = run_blackjax(
+        kernel,
+        rng, 
+        init_pos, 
+        input_dims,
+        num_samples, 
+        num_chains,
+        burn_in,
+        batch_fn
+    )
+
+    """Storing results"""
+    obj = {"fsm_times" : fsm_time,
+           "naive_times" : naive_time,
+           "fsm_ess" : fsm_ess,
+           "naive_ess" : naive_ess,
+           "fsm_logpdfcalls" : fsm_calls,
+           "naive_logpdfcalls" : naive_info.subiter.max(1).sum() * num_chains,
+           "iter_counts" : naive_info.subiter
+          }
+    
+    return obj
+
+if __name__ == "__main__":
+    main()
